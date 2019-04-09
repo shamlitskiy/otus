ac4256d8257fccb27d9d0e803d1aea4564dfbcbe
AlexSham <aleksey.shamlitskiy@onetwotrip.com>
2019-04-09 12:59:59 +0300

    Uncommit lines

--- homeworks/hw1/poker.py
+++ homeworks/hw1/poker.py
@@ -125,8 +125,8 @@ def best_wild_hand(hand):
 
 def test_best_hand():
     print "test_best_hand..."
-    # assert (sorted(best_hand("6C 7C 8C 9C TC 5C JS".split()))
-    #         == ['6C', '7C', '8C', '9C', 'TC'])
+    assert (sorted(best_hand("6C 7C 8C 9C TC 5C JS".split()))
+            == ['6C', '7C', '8C', '9C', 'TC'])
     assert (sorted(best_hand("TD TC TH 7C 7D 8C 8S".split()))
             == ['8C', '8S', 'TC', 'TD', 'TH'])
     assert (sorted(best_hand("JD TC TH 7C 7D 7S 7H".split()))
